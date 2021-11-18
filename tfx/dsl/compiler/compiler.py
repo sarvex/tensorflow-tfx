tensorflow/tfx

Naive ForEach compiler implementation using Unnest operator.
Long term direction: since interaction between Cond and ForEach is too complex to be encoded as ResolverSteps, we should encode context information directly (without using ResolverSteps) and let the input resolution stack (e.g. inputs_utils.py) determines the correct inputs based on the context information (e.g. nested Cond and ForEach).

PiperOrigin-RevId: 396531528

tfx/dsl/compiler/compiler.py
@@ -23,8 +23,13 @@ from tfx.dsl.components.base import base_component
 from tfx.dsl.components.base import base_driver
 from tfx.dsl.components.base import base_node
 from tfx.dsl.components.common import resolver
+from tfx.dsl.context_managers import context_manager
+from tfx.dsl.context_managers import for_each
 from tfx.dsl.experimental.conditionals import conditional
+from tfx.dsl.input_resolution import resolver_function
 from tfx.dsl.input_resolution import resolver_op
+from tfx.dsl.input_resolution.ops import skip_if_empty_op
+from tfx.dsl.input_resolution.ops import unnest_op
 from tfx.dsl.placeholder import placeholder
 from tfx.orchestration import data_types
 from tfx.orchestration import data_types_utils
@@ -204,7 +209,23 @@ class Compiler:
       resolver_step.config_json = json_utils.dumps(
           {"predicates": encoded_predicates})
 
-    # Step 3.2: Fill node inputs
+    # Step 3.2: Handle ForEach.
+    for dsl_context in context_manager.get_contexts(tfx_node):
+      if isinstance(dsl_context, for_each.ForEachContext):
+        for input_key, channel in tfx_node_inputs.items():
+          if (isinstance(channel, for_each.SlicedChannel) and
+              channel.wrapped is dsl_context.channel):
+            node.inputs.resolver_config.resolver_steps.extend(
+                _compile_for_each_context(input_key))
+            break
+        else:
+          # Ideally should not reach here as the same check is performed at
+          # ForEachContext.will_add_node().
+          raise ValueError(
+              f"Unable to locate ForEach loop varable {dsl_context.channel} "
+              f"from inputs of node {tfx_node.id}.")
+
+    # Step 3.3: Fill node inputs
     for key, value in itertools.chain(tfx_node_inputs.items(),
                                       implicit_input_channels.items()):
       input_spec = node.inputs.inputs[key]
@@ -254,11 +275,11 @@ class Compiler:
       # min_count = 0 stands for optional input and 1 stands for required input.
 
     # TODO(b/170694459): Refactor special nodes as plugins.
-    # Step 3.3: Special treatment for Resolver node.
+    # Step 3.4: Special treatment for Resolver node.
     if compiler_utils.is_resolver(tfx_node):
       assert compile_context.is_sync_mode
       node.inputs.resolver_config.resolver_steps.extend(
-          _convert_to_resolver_steps(tfx_node))
+          _compile_resolver_node(tfx_node))
 
     # Step 4: Node outputs
     for key, value in tfx_node.outputs.items():
@@ -458,7 +479,7 @@ class Compiler:
           input_channels[parent_input_key] = channel
       # Step 3.
       # Convert resolver node into corresponding resolver steps.
-      resolver_steps.extend(reversed(_convert_to_resolver_steps(resolver_node)))
+      resolver_steps.extend(reversed(_compile_resolver_node(resolver_node)))
 
     if resolver_steps:
       node.inputs.resolver_config.resolver_steps.extend(
@@ -547,20 +568,40 @@ def _compile_resolver_op(
   return result
 
 
-def _convert_to_resolver_steps(
-    resolver_node: base_node.BaseNode
+def _compile_resolver_function(
+    f: resolver_function.ResolverFunction,
+) -> List[pipeline_pb2.ResolverConfig.ResolverStep]:
+  """Converts ResolverFunction to corresponding ResolverSteps."""
+  result = []
+  op_node = f.trace()
+  while not op_node.is_input_node:
+    result.append(_compile_resolver_op(op_node))
+    op_node = op_node.arg
+  return list(reversed(result))
+
+
+def _compile_resolver_node(
+    resolver_node: base_node.BaseNode,
 ) -> List[pipeline_pb2.ResolverConfig.ResolverStep]:
   """Converts Resolver node to a corresponding ResolverSteps."""
   assert compiler_utils.is_resolver(resolver_node)
   resolver_node = cast(resolver.Resolver, resolver_node)
-  result = []
-  op_node = resolver_node.trace(resolver_op.OpNode.INPUT_NODE)
-  while not op_node.is_input_node:
-    result.append(_compile_resolver_op(op_node))
-    op_node = op_node.arg
+  result = _compile_resolver_function(resolver_node.resolver_function)
   for step in result:
     step.input_keys.extend(resolver_node.inputs.keys())
-  return list(reversed(result))
+  return result
+
+
+def _compile_for_each_context(
+    input_key: str) -> List[pipeline_pb2.ResolverConfig.ResolverStep]:
+  """Generates ResolverSteps corresponding to ForEach context."""
+
+  @resolver_function.resolver_function
+  def impl(input_node):
+    items = unnest_op.Unnest(input_node, key=input_key)
+    return skip_if_empty_op.SkipIfEmpty(items)
+
+  return _compile_resolver_function(impl)
 
 
 def _check_property_value_type(property_name: str,

tfx/dsl/compiler/testdata/foreach_pipeline.py
@@ -1,0 +1,73 @@
+# Copyright 2021 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Sample pipeline with ForEach context."""
+
+from tfx.components import CsvExampleGen
+from tfx.components import Pusher
+from tfx.components import SchemaGen
+from tfx.components import StatisticsGen
+from tfx.components import Trainer
+from tfx.dsl.components.common import resolver
+from tfx.dsl.context_managers import for_each
+from tfx.dsl.input_resolution.strategies import latest_artifact_strategy
+from tfx.orchestration import pipeline
+from tfx.proto import pusher_pb2
+from tfx.proto import trainer_pb2
+
+
+def create_test_pipeline():
+  """Creates a sample pipeline with ForEach context."""
+
+  example_gen = CsvExampleGen(input_base='/data/mydummy_dataset',)
+
+  with for_each.ForEach(example_gen.outputs['examples']) as examples:
+    statistics_gen = StatisticsGen(examples=examples)
+
+  latest_stats_resolver = resolver.Resolver(
+      statistics=statistics_gen.outputs['statistics'],
+      strategy_class=latest_artifact_strategy.LatestArtifactStrategy,
+  ).with_id('latest_stats_resolver')
+
+  schema_gen = SchemaGen(statistics=latest_stats_resolver.outputs['statistics'])
+
+  with for_each.ForEach(example_gen.outputs['examples']) as examples:
+    trainer = Trainer(
+        module_file='/src/train.py',
+        examples=examples,
+        schema=schema_gen.outputs['schema'],
+        train_args=trainer_pb2.TrainArgs(num_steps=2000),
+    )
+
+  with for_each.ForEach(trainer.outputs['model']) as model:
+    pusher = Pusher(
+        model=model,
+        push_destination=pusher_pb2.PushDestination(
+            filesystem=pusher_pb2.PushDestination.Filesystem(
+                base_directory='/models')),
+    )
+
+  return pipeline.Pipeline(
+      pipeline_name='foreach',
+      pipeline_root='/tfx/pipelines/foreach',
+      components=[
+          example_gen,
+          statistics_gen,
+          latest_stats_resolver,
+          schema_gen,
+          trainer,
+          pusher,
+      ],
+      enable_cache=True,
+      execution_mode=pipeline.ExecutionMode.SYNC,
+  )

sarvex/tensorflow-tfx

tfx/dsl/compiler/compiler.py
@@ -23,15 +23,19 @@ from tfx.dsl.components.base import base_component
 from tfx.dsl.components.base import base_driver
 from tfx.dsl.components.base import base_node
 from tfx.dsl.components.common import resolver
+from tfx.dsl.context_managers import context_manager
+from tfx.dsl.control_flow import for_each
 from tfx.dsl.experimental.conditionals import conditional
+from tfx.dsl.input_resolution import resolver_function
 from tfx.dsl.input_resolution import resolver_op
+from tfx.dsl.input_resolution.ops import skip_if_empty_op
+from tfx.dsl.input_resolution.ops import unnest_op
 from tfx.dsl.placeholder import placeholder
 from tfx.orchestration import data_types
 from tfx.orchestration import data_types_utils
 from tfx.orchestration import pipeline
 from tfx.proto.orchestration import executable_spec_pb2
 from tfx.proto.orchestration import pipeline_pb2
-from tfx.types import channel
 from tfx.types import channel_utils
 from tfx.utils import deprecation_utils
 from tfx.utils import json_utils
@@ -191,7 +195,7 @@ class Compiler:
     if predicates:
       implicit_keys_map = {}
       for key, chnl in tfx_node_inputs.items():
-        if not isinstance(chnl, channel.Channel):
+        if not isinstance(chnl, types.Channel):
           raise ValueError(
               "Conditional only support using channel as a predicate.")
         implicit_keys_map[compiler_utils.implicit_channel_key(chnl)] = key
@@ -212,7 +216,33 @@ class Compiler:
       resolver_step.config_json = json_utils.dumps(
           {"predicates": encoded_predicates})
 
-    # Step 3.2: Fill node inputs
+    # Step 3.2: Handle ForEach.
+    dsl_contexts = context_manager.get_contexts(tfx_node)
+    for dsl_context in dsl_contexts:
+      if isinstance(dsl_context, for_each.ForEachContext):
+        for input_key, channel in tfx_node_inputs.items():
+          if (isinstance(channel, types.LoopVarChannel) and
+              channel.wrapped is dsl_context.wrapped_channel):
+            node.inputs.resolver_config.resolver_steps.extend(
+                _compile_for_each_context(input_key))
+            break
+        else:
+          # Ideally should not reach here as the same check is performed at
+          # ForEachContext.will_add_node().
+          raise ValueError(
+              f"Unable to locate ForEach loop variable {dsl_context.channel} "
+              f"from inputs of node {tfx_node.id}.")
+
+    # Check loop variable is used outside the ForEach.
+    for input_key, channel in tfx_node_inputs.items():
+      if isinstance(channel, types.LoopVarChannel):
+        dsl_context_ids = {c.id for c in dsl_contexts}
+        if channel.context_id not in dsl_context_ids:
+          raise ValueError(
+              "Loop variable cannot be used outside the ForEach "
+              f"(node_id = {tfx_node.id}, input_key = {input_key}).")
+
+    # Step 3.3: Fill node inputs
     for key, value in itertools.chain(tfx_node_inputs.items(),
                                       implicit_input_channels.items()):
       input_spec = node.inputs.inputs[key]
@@ -284,11 +314,11 @@ class Compiler:
                 raise
 
     # TODO(b/170694459): Refactor special nodes as plugins.
-    # Step 3.3: Special treatment for Resolver node.
+    # Step 3.4: Special treatment for Resolver node.
     if compiler_utils.is_resolver(tfx_node):
       assert compile_context.is_sync_mode
       node.inputs.resolver_config.resolver_steps.extend(
-          _convert_to_resolver_steps(tfx_node))
+          _compile_resolver_node(tfx_node))
 
     # Step 4: Node outputs
     for key, value in tfx_node.outputs.items():
@@ -484,7 +514,7 @@ class Compiler:
           input_channels[parent_input_key] = chnl
       # Step 3.
       # Convert resolver node into corresponding resolver steps.
-      resolver_steps.extend(reversed(_convert_to_resolver_steps(resolver_node)))
+      resolver_steps.extend(reversed(_compile_resolver_node(resolver_node)))
 
     if resolver_steps:
       node.inputs.resolver_config.resolver_steps.extend(
@@ -572,20 +602,40 @@ def _compile_resolver_op(
   return result
 
 
-def _convert_to_resolver_steps(
-    resolver_node: base_node.BaseNode
+def _compile_resolver_function(
+    f: resolver_function.ResolverFunction,
+) -> List[pipeline_pb2.ResolverConfig.ResolverStep]:
+  """Converts ResolverFunction to corresponding ResolverSteps."""
+  result = []
+  op_node = f.trace()
+  while not op_node.is_input_node:
+    result.append(_compile_resolver_op(op_node))
+    op_node = op_node.arg
+  return list(reversed(result))
+
+
+def _compile_resolver_node(
+    resolver_node: base_node.BaseNode,
 ) -> List[pipeline_pb2.ResolverConfig.ResolverStep]:
   """Converts Resolver node to a corresponding ResolverSteps."""
   assert compiler_utils.is_resolver(resolver_node)
   resolver_node = cast(resolver.Resolver, resolver_node)
-  result = []
-  op_node = resolver_node.trace(resolver_op.OpNode.INPUT_NODE)
-  while not op_node.is_input_node:
-    result.append(_compile_resolver_op(op_node))
-    op_node = op_node.arg
+  result = _compile_resolver_function(resolver_node.resolver_function)
   for step in result:
     step.input_keys.extend(resolver_node.inputs.keys())
-  return list(reversed(result))
+  return result
+
+
+def _compile_for_each_context(
+    input_key: str) -> List[pipeline_pb2.ResolverConfig.ResolverStep]:
+  """Generates ResolverSteps corresponding to ForEach context."""
+
+  @resolver_function.resolver_function
+  def impl(input_node):
+    items = unnest_op.Unnest(input_node, key=input_key)
+    return skip_if_empty_op.SkipIfEmpty(items)
+
+  return _compile_resolver_function(impl)
 
 
 def _check_property_value_type(property_name: str,

tfx/dsl/compiler/testdata/foreach_pipeline.py
@@ -1,0 +1,73 @@
+# Copyright 2021 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Sample pipeline with ForEach context."""
+
+from tfx.components import CsvExampleGen
+from tfx.components import Pusher
+from tfx.components import SchemaGen
+from tfx.components import StatisticsGen
+from tfx.components import Trainer
+from tfx.dsl.components.common import resolver
+from tfx.dsl.control_flow import for_each
+from tfx.dsl.input_resolution.strategies import latest_artifact_strategy
+from tfx.orchestration import pipeline
+from tfx.proto import pusher_pb2
+from tfx.proto import trainer_pb2
+
+
+def create_test_pipeline():
+  """Creates a sample pipeline with ForEach context."""
+
+  example_gen = CsvExampleGen(input_base='/data/mydummy_dataset')
+
+  with for_each.ForEach(example_gen.outputs['examples']) as each_example:
+    statistics_gen = StatisticsGen(examples=each_example)
+
+  latest_stats_resolver = resolver.Resolver(
+      statistics=statistics_gen.outputs['statistics'],
+      strategy_class=latest_artifact_strategy.LatestArtifactStrategy,
+  ).with_id('latest_stats_resolver')
+
+  schema_gen = SchemaGen(statistics=latest_stats_resolver.outputs['statistics'])
+
+  with for_each.ForEach(example_gen.outputs['examples']) as each_example:
+    trainer = Trainer(
+        module_file='/src/train.py',
+        examples=each_example,
+        schema=schema_gen.outputs['schema'],
+        train_args=trainer_pb2.TrainArgs(num_steps=2000),
+    )
+
+  with for_each.ForEach(trainer.outputs['model']) as each_model:
+    pusher = Pusher(
+        model=each_model,
+        push_destination=pusher_pb2.PushDestination(
+            filesystem=pusher_pb2.PushDestination.Filesystem(
+                base_directory='/models')),
+    )
+
+  return pipeline.Pipeline(
+      pipeline_name='foreach',
+      pipeline_root='/tfx/pipelines/foreach',
+      components=[
+          example_gen,
+          statistics_gen,
+          latest_stats_resolver,
+          schema_gen,
+          trainer,
+          pusher,
+      ],
+      enable_cache=True,
+      execution_mode=pipeline.ExecutionMode.SYNC,
+  )